059955ce24cc04cfeddafa08ec8121017b3813da
MrMDWatson <mrmdwatson@gmail.com>
2024-05-28 18:24:20 -0400

    update env

--- 07QualityAssurance/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
+++ 07QualityAssurance/boilerplate-project-american-british-english-translator/tests/1_unit-tests.js
@@ -4,5 +4,47 @@ const assert = chai.assert;
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
-
+    suite("Test American to English translation", function() {
+        test("Test 1", function() {
+            assert.equal(Translator.translate("1"), 1);
+        });
+        /*
+        test("Test 2", function() {
+            assert.equal(solver.validate("1"), true);
+        });
+        test("Test 3", function() {
+            assert.equal(solver.validate("1").error, 'E');
+        });
+        test("Test 1", function() {
+            assert.isTrue(solver.validate("1"), true);
+        });
+        test("Test 2", function() {
+            assert.equal(solver.validate("1"), true);
+        });
+        test("Test 3", function() {
+            assert.equal(solver.validate("1").error, 'E');
+        });
+        test("Test 3", function() {
+            assert.equal(solver.validate("1").error, 'E');
+        });
+        */
+      });
+    /*
+      suite("Test row placement", function() {
+        test("Handles valid row placement", function() {
+          const puzzleString = "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+          const row = 0;
+          const column = 1;
+          const value = "3";
+          assert.isTrue(solver.checkRowPlacement(puzzleString, row, column, value));
+        });
+        test("Handles invalid row placement", function() {
+          const puzzleString = "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+          const row = 0;
+          const column = 1;
+          const value = "1";
+          assert.isFalse(solver.checkRowPlacement(puzzleString, row, column, value));
+        });
+      });
+      */
 });
